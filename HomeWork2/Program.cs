24e4dd2e4cb1729959d3da222b34d75fe33883f5
Станислав <@->
2017-08-21 15:59:28 +0300

    задача 2 не решена

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -10,6 +10,44 @@ namespace HomeWork2
     {
         static void Main(string[] args)
         {
+            /* 1. Написать программу, которая запрашивает 2 числа и выполняет их сложение и выводит результат.
+              Затем опять запрашивает 2 числа и выполняет вычитание. Повторить действия для умножения, 
+              деления и остатка от деления. Интерфейс должен быть максимально информативным для пользователя.*/
+
+            /* 
+            double num1;
+            double num2;
+
+
+            Console.Write("Enter number: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter number: ");
+            num2 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("\n" + num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+            Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+            Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+
+            if (num1 > num2)
+                Console.WriteLine(num1 + " % " + num2 + " = " + (num1 % num2));
+            else 
+                Console.WriteLine(num1 + " % " + num2 + " = False ");
+            
+            
+             */
+
+            // task 2.   (4/5)*x-20=0   x = ? 
+
+            
+            object x;
+            double eq = 0;
+
+            
+           
+
+                Console.ReadKey();
         }
     }
 }
